2a9505083aba2d45771853b1d5d817f05270ce36
jasonemarkson <jasonmarkson76@gmail.com>
2021-07-21 20:50:36 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,11 +6,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-    return movieData.map(movie => {[
-      <MovieCard CardFront={movie}/>,
-      <MovieCard CardBack={movie}/>
-      ]}
-    )
+      movieData.map(movie => <MovieCard CardFront/>)
   }
 
   render() {
